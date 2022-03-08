1c23a2947768825a79d8a909d53a02aafdaf5e04
Andrea Bianchi <bianan96@gmail.com>
2022-03-08 15:27:43 +0100

    adjust light theme

--- src/Theming.tsx
+++ src/Theming.tsx
@@ -1,14 +1,15 @@
 import "./Theming.scss";
-import { Moon, Speaker, Sun, Zap } from "react-feather";
+import { Moon, Sun } from "react-feather";
 
 export default function Theming() {
-  function setLightTheme() {
-    const vars = document.documentElement.style;
-    vars.setProperty("--color-bg", "rgb(255, 225, 148)");
-    vars.setProperty("--color-banner", "rgb(255, 179, 25)");
-    vars.setProperty("--color-primary", "rgb(232, 246, 239)");
-    vars.setProperty("--color-secondary", "rgb(184, 223, 216)");
-    vars.setProperty("--color-label", "rgb(255, 179, 25)");
+  const lightTheme = require("./themeConfig.json").light;
+  const darkTheme = require("./themeConfig.json").dark;
+
+  function setTheme(theme: any): void {
+    const colors = document.documentElement.style;
+    for (let key in theme) {
+      colors.setProperty(key, theme[key]);
+    }
   }
 
   return (
@@ -17,70 +18,31 @@ export default function Theming() {
         <p>I'm feeling...</p>
       </div>
       <form className='rating'>
-        <label htmlFor='Light'>
+        <label className='lightIcon' htmlFor='Light'>
           <input
             title='Set Light Theme'
             type='radio'
-            name='rating'
             className='light'
-            id='light'
             value='light'
-            onChange={setLightTheme}
+            onChange={() => {
+              setTheme(lightTheme);
+            }}
           />
-          <Sun
-            className='icon lightIcon'
-            color='var(--color-lightIcon)'
-            size={24}
-          />
+          <Sun color='var(--color-lightIcon)' size={24} />
         </label>
 
-        <label htmlFor='Dark'>
+        <label className='darkIcon' htmlFor='Dark'>
           <input
             title='Set Dark Theme'
             type='radio'
-            name='rating'
             className='dark'
-            id='dark'
             value='dark'
+            onChange={() => {
+              setTheme(darkTheme);
+            }}
             defaultChecked
           />
-          <Moon
-            className='icon darkIcon'
-            color='var(--color-darkIcon)'
-            size={24}
-          />
-        </label>
-
-        <label htmlFor='Groovy'>
-          <input
-            title='Set Groovy Theme'
-            type='radio'
-            name='rating'
-            className='groovy'
-            id='groovy'
-            value='groovy'
-          />
-          <Speaker
-            className='icon groovyIcon'
-            color='var(--color-groovyIcon'
-            size={24}
-          />
-        </label>
-
-        <label htmlFor='Speedy'>
-          <input
-            title='Set Speedy Theme'
-            type='radio'
-            name='rating'
-            className='speedy'
-            id='speedy'
-            value='speedy'
-          />
-          <Zap
-            className='icon speedyIcon'
-            color='var(--color-speedyIcon'
-            size={24}
-          />
+          <Moon color='var(--color-darkIcon)' size={24} />
         </label>
       </form>
     </div>
